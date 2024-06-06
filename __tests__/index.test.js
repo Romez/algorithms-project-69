c343241f02c05e0279fa79096a6710eb419e7d7b
roman_ushakov <roman.ushakov.89@inbox.ru>
2024-06-06 07:42:47 +0200

    step 3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,7 +11,7 @@ test('search docs', () => {
   const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
   const doc3 = { id: 'doc3', text: "I'm your shooter." };
   const docs = [doc1, doc2, doc3];
-  expect(search(docs, 'shoot')).toEqual(['doc1', 'doc2']);
+  expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1']);
 });
 
 test('search term', () => {
--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 const search = (docs, str) => {
   const terms = {};
+
   for (let i = 0; i < docs.length; i += 1) {
     const words = docs[i].text.split(' ');
     for (let j = 0; j < words.length; j += 1) {
       const term = words[j].match(/\w+/g);
       if (!terms[term]) {
-        terms[term] = [];
+        terms[term] = {};
       }
-      if (!terms[term].includes(docs[i])) {
-        terms[term].push(docs[i]);
+      const doc = docs[i];
+      const { id } = doc;
+
+      if (!terms[term][id]) {
+        terms[term][id] = { doc: docs[i], weight: 1 };
+      } else {
+        terms[term][id].weight += 1;
       }
     }
   }
 
   const strTerm = str.match(/\w+/g);
-  return terms[strTerm]?.map((doc) => doc.id) || [];
+
+  const termDocs = terms[strTerm];
+  if (!termDocs) {
+    return [];
+  }
+
+  const items = Object.values(termDocs);
+  items.sort((a, b) => {
+    if (a.weight > b.weight) {
+      return -1;
+    }
+    if (a.weight < b.weight) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return items.map(({ doc }) => doc.id) || [];
 };
 
 export default search;
