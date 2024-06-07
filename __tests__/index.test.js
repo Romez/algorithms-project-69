59d18372122f12e5805861392b87e7ac1f62906e
roman_ushakov <roman.ushakov.89@inbox.ru>
2024-06-07 08:33:37 +0200

    step 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,3 +20,12 @@ test('search term', () => {
   expect(search(docs, 'pint')).toEqual(['doc1']);
   expect(search(docs, 'pint!')).toEqual(['doc1']);
 });
+
+test('fuzzy search', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc3 = { id: 'doc3', text: "I'm your shooter." };
+  const docs = [doc1, doc2, doc3];
+
+  expect(search(docs, 'shoot at me')).toEqual(['doc2', 'doc1']);
+});
--- src/index.js
+++ src/index.js
@@ -1,30 +1,72 @@
-const search = (docs, str) => {
+const tokenize = (str) => str.match(/\w+/g).map((s) => s.toLowerCase());
+
+const search = (docs, query) => {
   const terms = {};
 
   for (let i = 0; i < docs.length; i += 1) {
-    const words = docs[i].text.split(' ');
-    for (let j = 0; j < words.length; j += 1) {
-      const term = words[j].match(/\w+/g);
+    const doc = docs[i];
+    const { id } = doc;
+    const docTerms = tokenize(docs[i].text);
+    for (let j = 0; j < docTerms.length; j += 1) {
+      const term = docTerms[j];
       if (!terms[term]) {
         terms[term] = {};
       }
-      const doc = docs[i];
-      const { id } = doc;
 
       if (!terms[term][id]) {
-        terms[term][id] = { doc: docs[i], weight: 1 };
+        terms[term][id] = { doc, weight: 1 };
       } else {
         terms[term][id].weight += 1;
       }
     }
   }
 
-  const strTerm = str.match(/\w+/g);
+  // terms {
+  //   i: {
+  //     doc1: { doc: [Object], weight: 2 },
+  //     doc3: { doc: [Object], weight: 1 }
+  //   },
+  //   can: { doc1: { doc: [Object], weight: 1 } },
+  //   t: {
+  //     doc1: { doc: [Object], weight: 1 },
+  //     doc2: { doc: [Object], weight: 1 }
+  //   },
+  //   shoot: {
+  //     doc1: { doc: [Object], weight: 1 },
+  //     doc2: { doc: [Object], weight: 3 }
+  //   },
+  //   straight: { doc1: { doc: [Object], weight: 1 } },
+  //   unless: { doc1: { doc: [Object], weight: 1 } },
+  //   ve: { doc1: { doc: [Object], weight: 1 } },
+  //   had: { doc1: { doc: [Object], weight: 1 } },
+  //   a: { doc1: { doc: [Object], weight: 1 } },
+  //   pint: { doc1: { doc: [Object], weight: 1 } },
+  //   don: { doc2: { doc: [Object], weight: 1 } },
+  //   that: { doc2: { doc: [Object], weight: 1 } },
+  //   thing: { doc2: { doc: [Object], weight: 1 } },
+  //   at: { doc2: { doc: [Object], weight: 1 } },
+  //   me: { doc2: { doc: [Object], weight: 1 } },
+  //   m: { doc3: { doc: [Object], weight: 1 } },
+  //   your: { doc3: { doc: [Object], weight: 1 } },
+  //   shooter: { doc3: { doc: [Object], weight: 1 } }
+  // }
 
-  const termDocs = terms[strTerm];
-  if (!termDocs) {
-    return [];
-  }
+  const queryTerms = tokenize(query);
+
+  const found = {};
+
+  const termDocs = queryTerms.reduce((acc, queryTerm) => {
+    if (!terms[queryTerm]) {
+      return acc;
+    }
+
+    return Object.entries(terms[queryTerm]).reduce((currAcc, [id, { doc, weight }]) => {
+      if (!currAcc[id] || currAcc[id].weight < weight) {
+        return { ...currAcc, [id]: { doc, weight } };
+      }
+      return currAcc;
+    }, acc);
+  }, {});
 
   const items = Object.values(termDocs);
   items.sort((a, b) => {
