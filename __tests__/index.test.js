e3465a8d05dbbeb3af593a88b957df5870fc9e38
roman_ushakov <roman.ushakov.89@inbox.ru>
2024-06-08 14:40:29 +0200

    fix 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,20 @@
 // @ts-check
+import path from 'path';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
 import { expect, test } from '@jest/globals';
 import search from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+
+const getDocumentText = async (id) => {
+  const documentPath = getFixturePath(id);
+  const text = await fs.readFile(documentPath, 'utf8');
+  return { id, text };
+};
 
 test('search empty', () => {
   expect(search([], 'shoot')).toEqual([]);
@@ -20,4 +34,44 @@ test('fuzzy search', () => {
   const docs = [doc1, doc2, doc3];
 
   expect(search(docs, 'shoot at me')).toEqual(['doc2', 'doc1']);
+});
+
+test('simple search', async () => {
+  const searchText = 'trash island';
+  const docIds = ['garbage_patch_NG', 'garbage_patch_ocean_clean', 'garbage_patch_wiki'];
+
+  const documentsPaths = await Promise.all(docIds.map(getDocumentText));
+  const result = await search(documentsPaths, searchText);
+
+  expect(result).toEqual(docIds);
+});
+
+test('search with spam, pretext', async () => {
+  const searchText = 'the trash island is a';
+  const docIds = [
+    'garbage_patch_NG',
+    'garbage_patch_ocean_clean',
+    'garbage_patch_wiki',
+    'garbage_patch_spam',
+  ];
+
+  const documentsPaths = await Promise.all(docIds.map(getDocumentText));
+  const result = await search(documentsPaths, searchText);
+
+  expect(result).toEqual(docIds);
+});
+
+test('short strings', async () => {
+  const doc1 = "I can't shoot straight unless I've had a pint!";
+  const doc2 = "Don't shoot shoot shoot that thing at me.";
+  const doc3 = "I'm your shooter.";
+  const docs = [
+    { id: 'doc1', text: doc1 },
+    { id: 'doc2', text: doc2 },
+    { id: 'doc3', text: doc3 },
+  ];
+
+  const result = await search(docs, 'shoot at me, nerd');
+
+  expect(result).toEqual(['doc2', 'doc1']);
 });
--- src/index.js
+++ src/index.js
@@ -1,63 +1,45 @@
-const tokenize = (text) => text.toLowerCase().match(/\w+/g).map((s) => s.toLowerCase());
+const tokenize = (text) => text.toLowerCase().match(/\b\w+\b/g);
 
 const search = (docs, query) => {
-  if (docs.length === 0) {
-    return [];
-  }
-  const index = {};
-  const weights = {};
-  const stats = {};
+  const reverseIndex = {}; // {term: [docId, ...]}
+  const termInDocIndex = {}; // {docId: {term: N}}
+  const termsCountInDoc = {}; // {docId: N}
 
   docs.forEach(({ id, text }) => {
     const terms = tokenize(text);
+    termsCountInDoc[id] = terms.length;
+    termInDocIndex[id] = {};
+
     terms.forEach((term) => {
-      if (!index[term]) {
-        index[term] = new Set();
+      if (!reverseIndex[term]) {
+        reverseIndex[term] = new Set();
       }
-      index[term].add(id);
-
-      if (!stats[term]) {
-        stats[term] = {};
+      if (!termInDocIndex[id][term]) {
+        termInDocIndex[id][term] = 0;
       }
-
-      if (!stats[term][id]) {
-        stats[term][id] = { total: terms.length, count: 0 };
-      }
-
-      stats[term][id].count += 1;
-    });
-  });
-
-  Object.keys(stats).forEach((term) => {
-    Object.keys(stats[term]).forEach((id) => {
-      const termStats = stats[term][id];
-      const tf = termStats.count / termStats.total;
-
-      const termCount = index[term].size;
-      const docsCount = docs.length;
-      const idf = Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5));
-
-      if (!weights[term]) {
-        weights[term] = 0;
-      }
-      weights[term] = Math.max(weights[term], tf * idf);
+      reverseIndex[term].add(id);
+      termInDocIndex[id][term] += 1;
     });
   });
 
   const queryTerms = tokenize(query);
 
-  const docsWithTerm = queryTerms.reduce((acc, term) => {
-    index[term].forEach((id) => {
-      if (!acc[id]) {
-        acc[id] = 0;
-      }
-      acc[id] += weights[term];
+  const weights = {}; // {term: 4}
+
+  queryTerms.forEach((term) => {
+    reverseIndex[term]?.forEach((id) => {
+      const termCountInDoc = termInDocIndex[id][term];
+      const docSize = termsCountInDoc[id];
+      const tf = termCountInDoc / docSize;
+
+      const termCount = reverseIndex[term].size;
+      const docsCount = docs.length;
+      const idf = Math.log(1 + (docsCount - termCount + 1) / (termCount + 0.5));
+      weights[id] = (weights[id] || 0) + (tf * idf);
     });
-    return acc;
-  }, {});
+  });
 
-  const items = Object.entries(docsWithTerm);
-
+  const items = Object.entries(weights);
   items.sort((a, b) => (b[1] - a[1]));
 
   return items.map((a) => a[0]) || [];
